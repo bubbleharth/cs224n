18687c99c144d7806e62e2505088c055758c741f
Jasper Lin <jasperlin451@gmail.com>
2015-11-30 18:04:18 -0800

    fix scanning in

--- pa4/pa4/java/src/cs224n/deep/FeatureFactory.java
+++ pa4/pa4/java/src/cs224n/deep/FeatureFactory.java
@@ -8,6 +8,8 @@ import org.ejml.simple.*;
 
 
 public class FeatureFactory {
+	private static final int numRows = 100232; // Number of word vectors.
+	private static final int numCols = 50; // Number of dimensions.
 
 
 	private FeatureFactory() {
@@ -19,10 +21,6 @@ public class FeatureFactory {
 	
 	public static List<Datum> getTestData() {
 		return testData;
-	}
-	
-	public static SimpleMatrix getWordVectors() {
-		return allVecs;
 	}
 	
 	public static HashMap<String, Integer> getDictionary() {
@@ -70,54 +68,43 @@ public class FeatureFactory {
 
 	// Look up table matrix with all word vectors as defined in lecture with dimensionality n x |V|
 	static SimpleMatrix allVecs; //access it directly in WindowModel
-	public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
-		if (allVecs!=null) return allVecs;
-
-        int numOfWords = wordToNum.size();
-        int numOfDim = 1;
-
-        double [][] data = null;
-        BufferedReader br = new BufferedReader(new FileReader(vecFilename));
-        int col = 0;
-        for (String line = br.readLine(); line != null; line = br.readLine()){
-            if (line.trim().length() == 0){
-                continue;
-            }
-            String [] words = line.split("\\s+");
-            if (data == null) {
-                numOfDim = words.length;
-                data = new double[numOfDim][numOfWords];
-            }
-            for (int row = 0; row < numOfDim; row++){
-                data[row][col] = Double.parseDouble(words[row]);
-            }
-            col++;
-        }
-        br.close();
-        allVecs = new SimpleMatrix(data);
-        return allVecs;
-
-	}
+	  public static SimpleMatrix readWordVectors(String vecFilename) throws IOException {
+		    if (allVecs != null) return allVecs;
+		    double[][] mat = new double[numRows][numCols];
+		    BufferedReader in = new BufferedReader(new FileReader(vecFilename));
+		    int row = 0;
+		    for (String line = in.readLine(); line != null; line = in.readLine()) {
+		      String[] nums = line.split("\\s+");
+		      if (nums.length != numCols) {
+		        throw new RuntimeException("dimension does not match");
+		      }
+		      for (int col = 0; col < numCols; col++) {
+		        mat[row][col] = Double.parseDouble(nums[col]);
+		      }
+		      row++;
+		    }
+		    if (row != numRows) {
+		      throw new RuntimeException("dimension does not match");
+		    }
+		    in.close();
+		    allVecs = new SimpleMatrix(mat);
+		    return allVecs;
+		  }
+	  
 	// might be useful for word to number lookups, just access them directly in WindowModel
 	public static HashMap<String, Integer> wordToNum = new HashMap<String, Integer>();
 	public static HashMap<Integer, String> numToWord = new HashMap<Integer, String>();
 
 	public static HashMap<String, Integer> initializeVocab(String vocabFilename) throws IOException {
-		//TODO: create this
-        if (wordToNum.size() != 0)
-		    return wordToNum;
-        BufferedReader br = new BufferedReader(new FileReader(vocabFilename));
-        int index = 0;
-        for (String line = br.readLine(); line != null; line = br.readLine()){
-            if (line.trim().length() == 0) {
-                continue;
-            }
-            String [] words = line.split("\\s+");
-            wordToNum.put(words[0], index);
-            numToWord.put(index, words[0]);
-            index++;
-        }
-        br.close();
-        return wordToNum;
-	}
+	    BufferedReader in = new BufferedReader(new FileReader(vocabFilename));
+	    int i = 0;
+	    for (String line = in.readLine(); line != null; line = in.readLine()) {
+	      String word = line.trim();
+	      wordToNum.put(word, i);
+	      numToWord.put(i, word);
+	      i++;
+	    }
+	    in.close();
+	    return wordToNum;
+	  }
 }
